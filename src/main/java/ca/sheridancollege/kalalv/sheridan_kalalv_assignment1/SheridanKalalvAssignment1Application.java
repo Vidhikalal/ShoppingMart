960083db23e195b55cc42a1337a7d621e8943f6b
Vidhi Kalal <kalalv@sheridancollege.ca>
2024-02-05 14:52:56 -0500

    Initialised project

---
+++ src/main/java/ca/sheridancollege/kalalv/sheridan_kalalv_assignment1/SheridanKalalvAssignment1Application.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.kalalv.sheridan_kalalv_assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheridanKalalvAssignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheridanKalalvAssignment1Application.class, args);
+    }
+
+}
