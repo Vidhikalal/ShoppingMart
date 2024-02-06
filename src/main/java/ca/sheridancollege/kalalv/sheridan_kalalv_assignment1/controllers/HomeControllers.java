e0fa4f7d272e5492e2824252889cf32c66d2f834
Vidhi Kalal <kalalv@sheridancollege.ca>
2024-02-06 11:50:53 -0500

    Made some changes in product.html

--- src/main/java/ca/sheridancollege/kalalv/sheridan_kalalv_assignment1/controllers/HomeControllers.java
+++ src/main/java/ca/sheridancollege/kalalv/sheridan_kalalv_assignment1/controllers/HomeControllers.java
@@ -2,11 +2,16 @@ package ca.sheridancollege.kalalv.sheridan_kalalv_assignment1.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeControllers {
   @GetMapping("/")
    public String index(){
        return "index";
+   }
+   @GetMapping("/product")
+    public String product(){
+      return "product";
    }
 }
